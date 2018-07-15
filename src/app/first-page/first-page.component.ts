3abda95df6ee02059861514d8964a476c1d3ebcf
CaesarIV <mostafa.beheiry.studios@gmail.com>
2018-07-15 18:49:10 +0200

    Fixed Anon Issue

--- src/app/first-page/first-page.component.ts
+++ src/app/first-page/first-page.component.ts
@@ -20,10 +20,10 @@ export class FirstPageComponent implements OnInit {
   @Input() userID: string;
   @Output() userIDchange:EventEmitter<string> = new EventEmitter<string>();
   oldId: string;
-  changeId: string;
+  changeId = "Anon";
   fixedId: string;
   newId: string;
-  ageGroup: number;
+  ageGroup = -1;
   phones = new FormControl();  
   phonesList: string[] = ['Android Operated', 'iPhone', 'Windows Phone', 'Other'];
   arExpText = '';
@@ -51,14 +51,41 @@ export class FirstPageComponent implements OnInit {
   }
 
   submitBackground(){        
-    this.backgroundQuestions[0].name = this.changeId;
-    this.backgroundQuestions[0].phone = this.phones.value;
-    this.backgroundQuestions[0].expArHw = this.arWare.value;
-    this.backgroundQuestions[0].ageGroup = this.ageGroup+"";
-    this.backgroundQuestions[0].expAr = this.expWithAr;
-    this.backgroundQuestions[0].expArMore = this.arExpText;
-    
-    this.afs.collection('participants').doc(this.newId).collection('backgroundAnswers').doc("background").set({...this.backgroundQuestions});    
-  }
+    if (!(typeof this.changeId === "undefined")){ 
+      this.backgroundQuestions[0].name = this.changeId;
+    }else{
+      this.backgroundQuestions[0].name = "Anon";
+    }
 
+    if (!(typeof this.phones === "undefined")) 
+    this.backgroundQuestions[0].phone = this.phones.value;
+
+    if (!(typeof this.arWare === "undefined")) 
+    this.backgroundQuestions[0].expArHw = this.arWare.value;
+
+    if (!(typeof this.ageGroup === "undefined")){ 
+      this.backgroundQuestions[0].ageGroup = this.ageGroup+"";
+    }else{
+      this.backgroundQuestions[0].ageGroup = "Anon";
+    }
+
+    if (!(typeof this.expWithAr === "undefined")) 
+    this.backgroundQuestions[0].expAr = this.expWithAr;
+
+    if (!(typeof this.arExpText === "undefined")) 
+    this.backgroundQuestions[0].expArMore = this.arExpText;
+
+    console.log("Type of ID: " + (typeof this.changeId));
+    console.log("Type of Phone: " + (typeof this.phones));
+    console.log("Type of ArWare: " + (typeof this.arWare));
+    console.log("Type of AgeGroup: " + (typeof this.ageGroup));
+    console.log("Type of Exp: " + (typeof this.expWithAr));
+    console.log("Type of IarExpTextD: " + (typeof this.arExpText));    
+    
+    if (!(typeof this.newId === "undefined")){ 
+      this.afs.collection('participants').doc(this.newId).collection('backgroundAnswers').doc("background").set({...this.backgroundQuestions});    
+    }else{
+      this.afs.collection('participants').doc(this.fixedId).collection('backgroundAnswers').doc("background").set({...this.backgroundQuestions});
+    }
+  }
 }
